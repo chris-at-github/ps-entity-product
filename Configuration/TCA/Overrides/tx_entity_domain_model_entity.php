f180b76de50acb8cb752b4cd1229808f78f7f452
Christian Pschorr <c.pschorr@datamints.com>
2020-11-23 23:31:41 +0100

    Neues Feld MasterCategory im TCA definiert

--- Configuration/TCA/Overrides/tx_entity_domain_model_entity.php
+++ Configuration/TCA/Overrides/tx_entity_domain_model_entity.php
@@ -5,5 +5,5 @@ if(isset($GLOBALS['TCA']['tx_entity_domain_model_entity']['columns']['tx_extbase
 	$GLOBALS['TCA']['tx_entity_domain_model_entity']['columns']['tx_extbase_type']['config']['items'][] = ['LLL:EXT:entity_product/Resources/Private/Language/locallang_tca.xlf:tx_entity_domain_model_entity.tx_extbase_type.product','Ps\EntityProduct\Domain\Model\Product'];
 }
 
-$GLOBALS['TCA']['tx_entity_domain_model_entity']['types']['Ps\EntityProduct\Domain\Model\Product']['showitem'] = 'sys_language_uid, l10n_parent, l10n_diffsource, tx_extbase_type, title, slug, 
+$GLOBALS['TCA']['tx_entity_domain_model_entity']['types']['Ps\EntityProduct\Domain\Model\Product']['showitem'] = 'sys_language_uid, l10n_parent, l10n_diffsource, tx_extbase_type, title, slug, master_category, 
 	--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.access, hidden, starttime, endtime';
