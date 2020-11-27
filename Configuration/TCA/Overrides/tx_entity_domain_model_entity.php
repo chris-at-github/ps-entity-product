b427a3581d3f48d3eb250354e08f0cc2743b301c
Christian Pschorr <c.pschorr@datamints.com>
2020-11-27 22:09:05 +0100

    Aufräumarbeiten TCA

--- Configuration/TCA/Overrides/tx_entity_domain_model_entity.php
+++ Configuration/TCA/Overrides/tx_entity_domain_model_entity.php
@@ -1,169 +1,159 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-if(isset($GLOBALS['TCA']['tx_entity_domain_model_entity']['columns']['tx_extbase_type']) === true) {
-	$GLOBALS['TCA']['tx_entity_domain_model_entity']['columns']['tx_extbase_type']['config']['items'][] = ['LLL:EXT:entity_product/Resources/Private/Language/locallang_tca.xlf:tx_entity_domain_model_entity.tx_extbase_type.product','Ps\EntityProduct\Domain\Model\Product'];
+if (isset($GLOBALS['TCA']['tx_entity_domain_model_entity']['columns']['tx_extbase_type']) === true) {
+	$GLOBALS['TCA']['tx_entity_domain_model_entity']['columns']['tx_extbase_type']['config']['items'][] = ['LLL:EXT:entity_product/Resources/Private/Language/locallang_tca.xlf:tx_entity_domain_model_entity.tx_extbase_type.product', 'Ps\EntityProduct\Domain\Model\Product'];
 }
 
-$tmp_entity_product_columns = [
-
-
-    'technical_drawings' => [
-        'exclude' => true,
-        'label' => 'LLL:EXT:entity_product/Resources/Private/Language/locallang_db.xlf:tx_entityproduct_domain_model_product.technical_drawings',
-        'config' =>
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getFileFieldTCAConfig(
-            'technical_drawings',
-            [
-                'appearance' => [
-                    'createNewRelationLinkTitle' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:images.addFileReference'
-                ],
-                'foreign_types' => [
-                    '0' => [
-                        'showitem' => '
-                        --palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
-                        --palette--;;filePalette'
-                    ],
-                    \TYPO3\CMS\Core\Resource\File::FILETYPE_TEXT => [
-                        'showitem' => '
-                        --palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
-                        --palette--;;filePalette'
-                    ],
-                    \TYPO3\CMS\Core\Resource\File::FILETYPE_IMAGE => [
-                        'showitem' => '
-                        --palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
-                        --palette--;;filePalette'
-                    ],
-                    \TYPO3\CMS\Core\Resource\File::FILETYPE_AUDIO => [
-                        'showitem' => '
-                        --palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
-                        --palette--;;filePalette'
-                    ],
-                    \TYPO3\CMS\Core\Resource\File::FILETYPE_VIDEO => [
-                        'showitem' => '
-                        --palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
-                        --palette--;;filePalette'
-                    ],
-                    \TYPO3\CMS\Core\Resource\File::FILETYPE_APPLICATION => [
-                        'showitem' => '
-                        --palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
-                        --palette--;;filePalette'
-                    ]
-                ],
-                'foreign_match_fields' => [
-                    'fieldname' => 'technical_drawings',
-                    'tablenames' => 'tx_entity_domain_model_entity',
-                    'table_local' => 'sys_file',
-                ],
-                'maxitems' => 99
-            ],
-            $GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext']
-        ),
-
-
-
-    ],
-    'applications' => [
-        'exclude' => true,
-        'label' => 'LLL:EXT:entity_product/Resources/Private/Language/locallang_db.xlf:tx_entityproduct_domain_model_product.applications',
-			'config' => [
-				'type' => 'select',
-				'renderType' => 'selectMultipleSideBySide',
-				'foreign_table' => 'pages',
-				'MM' => 'tx_entityproduct_product_applications_mm',
-				'size' => 10,
-				'autoSizeMax' => 30,
-				'maxitems' => 9999,
-				'multiple' => 0,
-				'fieldControl' => [
-					'editPopup' => [
-						'disabled' => false,
+$tmpEntityProductColumns = [
+	'technical_drawings' => [
+		'exclude' => true,
+		'label' => 'LLL:EXT:entity_product/Resources/Private/Language/locallang_db.xlf:tx_entityproduct_domain_model_product.technical_drawings',
+		'config' =>
+			\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getFileFieldTCAConfig(
+				'technical_drawings',
+				[
+					'appearance' => [
+						'createNewRelationLinkTitle' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:images.addFileReference'
 					],
-					'addRecord' => [
-						'disabled' => false,
+					'foreign_types' => [
+						'0' => [
+							'showitem' => '
+                        --palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
+                        --palette--;;filePalette'
+						],
+						\TYPO3\CMS\Core\Resource\File::FILETYPE_TEXT => [
+							'showitem' => '
+                        --palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
+                        --palette--;;filePalette'
+						],
+						\TYPO3\CMS\Core\Resource\File::FILETYPE_IMAGE => [
+							'showitem' => '
+                        --palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
+                        --palette--;;filePalette'
+						],
+						\TYPO3\CMS\Core\Resource\File::FILETYPE_AUDIO => [
+							'showitem' => '
+                        --palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
+                        --palette--;;filePalette'
+						],
+						\TYPO3\CMS\Core\Resource\File::FILETYPE_VIDEO => [
+							'showitem' => '
+                        --palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
+                        --palette--;;filePalette'
+						],
+						\TYPO3\CMS\Core\Resource\File::FILETYPE_APPLICATION => [
+							'showitem' => '
+                        --palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
+                        --palette--;;filePalette'
+						]
 					],
-					'listModule' => [
-						'disabled' => true,
+					'foreign_match_fields' => [
+						'fieldname' => 'technical_drawings',
+						'tablenames' => 'tx_entity_domain_model_entity',
+						'table_local' => 'sys_file',
 					],
+					'maxitems' => 99
+				],
+				$GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext']
+			),
+	],
+	'applications' => [
+		'exclude' => true,
+		'label' => 'LLL:EXT:entity_product/Resources/Private/Language/locallang_db.xlf:tx_entityproduct_domain_model_product.applications',
+		'config' => [
+			'type' => 'select',
+			'renderType' => 'selectMultipleSideBySide',
+			'foreign_table' => 'pages',
+			'MM' => 'tx_entityproduct_product_applications_mm',
+			'size' => 10,
+			'autoSizeMax' => 30,
+			'maxitems' => 9999,
+			'multiple' => 0,
+			'fieldControl' => [
+				'editPopup' => [
+					'disabled' => false,
+				],
+				'addRecord' => [
+					'disabled' => false,
+				],
+				'listModule' => [
+					'disabled' => true,
 				],
 			],
-    ],
-    'attributes' => [
-        'exclude' => true,
-        'label' => 'LLL:EXT:entity_product/Resources/Private/Language/locallang_db.xlf:tx_entityproduct_domain_model_product.attributes',
-        'config' => [
-            'type' => 'select',
-            'renderType' => 'selectMultipleSideBySide',
-            'foreign_table' => 'tx_entityproduct_domain_model_attribute',
-            'MM' => 'tx_entityproduct_product_attribute_mm',
-            'size' => 10,
-            'autoSizeMax' => 30,
-            'maxitems' => 9999,
-            'multiple' => 0,
-            'fieldControl' => [
-                'editPopup' => [
-                    'disabled' => false,
-                ],
-                'addRecord' => [
-                    'disabled' => false,
-                ],
-                'listModule' => [
-                    'disabled' => true,
-                ],
-            ],
-        ],
-
-    ],
-    'variants' => [
-        'exclude' => true,
-        'label' => 'LLL:EXT:entity_product/Resources/Private/Language/locallang_db.xlf:tx_entityproduct_domain_model_product.variants',
-        'config' => [
-            'type' => 'inline',
-            'foreign_table' => 'tx_entityproduct_domain_model_variant',
-            'foreign_field' => 'product',
-            'maxitems' => 9999,
-            'appearance' => [
-                'collapseAll' => 0,
-                'levelLinksPosition' => 'top',
-                'showSynchronizationLink' => 1,
-                'showPossibleLocalizationRecords' => 1,
-                'showAllLocalizationLink' => 1
-            ],
-        ],
-
-
-    ],
-    'accessories' => [
-        'exclude' => true,
-        'label' => 'LLL:EXT:entity_product/Resources/Private/Language/locallang_db.xlf:tx_entityproduct_domain_model_product.accessories',
-        'config' => [
-            'type' => 'select',
-            'renderType' => 'selectMultipleSideBySide',
-            'foreign_table' => 'tx_entity_domain_model_entity',
-            'MM' => 'tx_entityproduct_product_accessories_mm',
-            'size' => 10,
-            'autoSizeMax' => 30,
-            'maxitems' => 9999,
-            'multiple' => 0,
-            'fieldControl' => [
-                'editPopup' => [
-                    'disabled' => false,
-                ],
-                'addRecord' => [
-                    'disabled' => false,
-                ],
-                'listModule' => [
-                    'disabled' => true,
-                ],
-            ],
-        ],
-
-    ],
-
+		],
+	],
+	'attributes' => [
+		'exclude' => true,
+		'label' => 'LLL:EXT:entity_product/Resources/Private/Language/locallang_db.xlf:tx_entityproduct_domain_model_product.attributes',
+		'config' => [
+			'type' => 'select',
+			'renderType' => 'selectMultipleSideBySide',
+			'foreign_table' => 'tx_entityproduct_domain_model_attribute',
+			'MM' => 'tx_entityproduct_product_attribute_mm',
+			'size' => 10,
+			'autoSizeMax' => 30,
+			'maxitems' => 9999,
+			'multiple' => 0,
+			'fieldControl' => [
+				'editPopup' => [
+					'disabled' => false,
+				],
+				'addRecord' => [
+					'disabled' => false,
+				],
+				'listModule' => [
+					'disabled' => true,
+				],
+			],
+		],
+	],
+	'variants' => [
+		'exclude' => true,
+		'label' => 'LLL:EXT:entity_product/Resources/Private/Language/locallang_db.xlf:tx_entityproduct_domain_model_product.variants',
+		'config' => [
+			'type' => 'inline',
+			'foreign_table' => 'tx_entityproduct_domain_model_variant',
+			'foreign_field' => 'product',
+			'maxitems' => 9999,
+			'appearance' => [
+				'collapseAll' => 0,
+				'levelLinksPosition' => 'top',
+				'showSynchronizationLink' => 1,
+				'showPossibleLocalizationRecords' => 1,
+				'showAllLocalizationLink' => 1
+			],
+		],
+	],
+	'accessories' => [
+		'exclude' => true,
+		'label' => 'LLL:EXT:entity_product/Resources/Private/Language/locallang_db.xlf:tx_entityproduct_domain_model_product.accessories',
+		'config' => [
+			'type' => 'select',
+			'renderType' => 'selectMultipleSideBySide',
+			'foreign_table' => 'tx_entity_domain_model_entity',
+			'MM' => 'tx_entityproduct_product_accessories_mm',
+			'size' => 10,
+			'autoSizeMax' => 30,
+			'maxitems' => 9999,
+			'multiple' => 0,
+			'fieldControl' => [
+				'editPopup' => [
+					'disabled' => false,
+				],
+				'addRecord' => [
+					'disabled' => false,
+				],
+				'listModule' => [
+					'disabled' => true,
+				],
+			],
+		],
+	],
 ];
 
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tx_entity_domain_model_entity', $tmp_entity_product_columns);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tx_entity_domain_model_entity', $tmpEntityProductColumns);
 
 
 $GLOBALS['TCA']['tx_entity_domain_model_entity']['types']['Ps\EntityProduct\Domain\Model\Product']['showitem'] = 'sys_language_uid, l10n_parent, l10n_diffsource,
