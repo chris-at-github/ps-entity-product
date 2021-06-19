baed375008694e550f6eb551d39661c8767dbd58
Christian Pschorr <c.pschorr@datamints.com>
2021-06-19 18:00:54 +0200

    WhitelistProvider für den Produktfilter auf die Mehrfachzuordnung von Kategorien angepasst

--- Classes/Provider/CategoryWhitelistDataProvider.php
+++ Classes/Provider/CategoryWhitelistDataProvider.php
@@ -16,10 +16,37 @@ class CategoryWhitelistDataProvider extends \Ps\Xo\Filter\DataProvider\AbstractD
 	public function provide($data, $properties) {
 		$settings = $this->getFilter()->getSettings();
 		$whitelist = [];
+		$entities = [];
 
 		if(isset($settings['productRange']) === true) {
 
+			// Erstmal alle Produkte identifizieren, die mit dieser Produktgruppe verknuepft sind
 			/** @var \TYPO3\CMS\Core\Database\Query\QueryBuilder  $queryBuilder */
+			$queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getConnectionForTable('tx_entity_domain_model_entity')->createQueryBuilder();
+			$query = $queryBuilder->select('tx_entity_domain_model_entity.uid')
+				->from('tx_entity_domain_model_entity')
+				->innerJoin(
+					'tx_entity_domain_model_entity',
+					'sys_category_record_mm',
+					'sys_category_record_mm',
+					$queryBuilder->expr()->eq('sys_category_record_mm.uid_foreign', $queryBuilder->quoteIdentifier('tx_entity_domain_model_entity.uid'))
+				)
+				->where(
+					$queryBuilder->expr()->in('tx_entity_domain_model_entity.sys_language_uid', [0, -1]),
+					$queryBuilder->expr()->orX(
+						$queryBuilder->expr()->eq('tx_entity_domain_model_entity.master_category', $queryBuilder->createNamedParameter($settings['productRange'], \PDO::PARAM_INT)),
+						$queryBuilder->expr()->eq('sys_category_record_mm.uid_local', $queryBuilder->createNamedParameter($settings['productRange'], \PDO::PARAM_INT))
+					)
+				)
+				->groupBy('tx_entity_domain_model_entity.uid');
+
+			$statement = $query->execute();
+
+			while($row = $statement->fetch()) {
+				$entities[] = (int) $row['uid'];
+			}
+
+			// Jetzt alle Kategorien laden, die zu den Produkten passen
 			$queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getConnectionForTable('tx_entity_domain_model_entity')->createQueryBuilder();
 			$query = $queryBuilder->select('sys_category.parent', 'sys_category_record_mm.uid_local')
 				->from('tx_entity_domain_model_entity')
@@ -37,11 +64,10 @@ class CategoryWhitelistDataProvider extends \Ps\Xo\Filter\DataProvider\AbstractD
 				)
 				->where(
 					$queryBuilder->expr()->in('tx_entity_domain_model_entity.sys_language_uid', [0, -1]),
-					$queryBuilder->expr()->eq('tx_entity_domain_model_entity.master_category', $queryBuilder->createNamedParameter($settings['productRange'], \PDO::PARAM_INT)),
 					$queryBuilder->expr()->eq('sys_category_record_mm.tablenames', $queryBuilder->createNamedParameter('tx_entity_domain_model_entity', \PDO::PARAM_STR)),
-					$queryBuilder->expr()->eq('sys_category_record_mm.fieldname', $queryBuilder->createNamedParameter('categories', \PDO::PARAM_STR))
+					$queryBuilder->expr()->eq('sys_category_record_mm.fieldname', $queryBuilder->createNamedParameter('categories', \PDO::PARAM_STR)),
+					$queryBuilder->expr()->in('tx_entity_domain_model_entity.uid', $entities)
 				);
-
 
 			$statement = $query->execute();
 
