54f8bd24ffffa5e4a64d3f26f2bf8c8b322a3064
Christian Pschorr <c.pschorr@datamints.com>
2021-06-20 18:30:46 +0200

    Fix für Produktreihen ohne ausgewählte Filtereinträge

--- Classes/Provider/CategoryWhitelistDataProvider.php
+++ Classes/Provider/CategoryWhitelistDataProvider.php
@@ -47,33 +47,35 @@ class CategoryWhitelistDataProvider extends \Ps\Xo\Filter\DataProvider\AbstractD
 			}
 
 			// Jetzt alle Kategorien laden, die zu den Produkten passen
-			$queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getConnectionForTable('tx_entity_domain_model_entity')->createQueryBuilder();
-			$query = $queryBuilder->select('sys_category.parent', 'sys_category_record_mm.uid_local')
-				->from('tx_entity_domain_model_entity')
-				->innerJoin(
-					'tx_entity_domain_model_entity',
-					'sys_category_record_mm',
-					'sys_category_record_mm',
-					$queryBuilder->expr()->eq('sys_category_record_mm.uid_foreign', $queryBuilder->quoteIdentifier('tx_entity_domain_model_entity.uid'))
-				)
-				->innerJoin(
-					'sys_category_record_mm',
-					'sys_category',
-					'sys_category',
-					$queryBuilder->expr()->eq('sys_category.uid', $queryBuilder->quoteIdentifier('sys_category_record_mm.uid_local'))
-				)
-				->where(
-					$queryBuilder->expr()->in('tx_entity_domain_model_entity.sys_language_uid', [0, -1]),
-					$queryBuilder->expr()->eq('sys_category_record_mm.tablenames', $queryBuilder->createNamedParameter('tx_entity_domain_model_entity', \PDO::PARAM_STR)),
-					$queryBuilder->expr()->eq('sys_category_record_mm.fieldname', $queryBuilder->createNamedParameter('categories', \PDO::PARAM_STR)),
-					$queryBuilder->expr()->in('tx_entity_domain_model_entity.uid', $entities)
-				);
+			if(empty($entities) === false) {
+				$queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getConnectionForTable('tx_entity_domain_model_entity')->createQueryBuilder();
+				$query = $queryBuilder->select('sys_category.parent', 'sys_category_record_mm.uid_local')
+					->from('tx_entity_domain_model_entity')
+					->innerJoin(
+						'tx_entity_domain_model_entity',
+						'sys_category_record_mm',
+						'sys_category_record_mm',
+						$queryBuilder->expr()->eq('sys_category_record_mm.uid_foreign', $queryBuilder->quoteIdentifier('tx_entity_domain_model_entity.uid'))
+					)
+					->innerJoin(
+						'sys_category_record_mm',
+						'sys_category',
+						'sys_category',
+						$queryBuilder->expr()->eq('sys_category.uid', $queryBuilder->quoteIdentifier('sys_category_record_mm.uid_local'))
+					)
+					->where(
+						$queryBuilder->expr()->in('tx_entity_domain_model_entity.sys_language_uid', [0, -1]),
+						$queryBuilder->expr()->eq('sys_category_record_mm.tablenames', $queryBuilder->createNamedParameter('tx_entity_domain_model_entity', \PDO::PARAM_STR)),
+						$queryBuilder->expr()->eq('sys_category_record_mm.fieldname', $queryBuilder->createNamedParameter('categories', \PDO::PARAM_STR)),
+						$queryBuilder->expr()->in('tx_entity_domain_model_entity.uid', $entities)
+					);
 
-			$statement = $query->execute();
+				$statement = $query->execute();
 
-			while($row = $statement->fetch()) {
-				$whitelist[$row['parent']] = (int) $row['parent'];
-				$whitelist[$row['uid_local']] = (int) $row['uid_local'];
+				while($row = $statement->fetch()) {
+					$whitelist[$row['parent']] = (int) $row['parent'];
+					$whitelist[$row['uid_local']] = (int) $row['uid_local'];
+				}
 			}
 		}
 
