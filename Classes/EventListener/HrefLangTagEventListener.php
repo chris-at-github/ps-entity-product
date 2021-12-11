fb6fc368344be556e48c235be336d4dae4c16255
Christian Pschorr <c.pschorr@datamints.com>
2021-12-11 22:36:20 +0100

    URL Generierung für Entities überarbeitet -> Generierung kann über Option für Zielseite und Sprache angepasst werden

--- Classes/EventListener/HrefLangTagEventListener.php
+++ Classes/EventListener/HrefLangTagEventListener.php
@@ -2,6 +2,8 @@
 
 namespace Ps\EntityProduct\EventListener;
 
+use Ps\Entity\Domain\Model\Entity;
+use Ps\EntityProduct\Domain\Repository\ProductRepository;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class HrefLangTagEventListener extends \Ps\Entity\EventListener\HrefLangTagEventListener {
@@ -17,5 +19,13 @@ class HrefLangTagEventListener extends \Ps\Entity\EventListener\HrefLangTagEvent
 		}
 
 		return null;
+	}
+
+	/**
+	 * @param int $uid
+	 * @return Entity
+	 */
+	protected function getEntity($uid) {
+		return GeneralUtility::makeInstance(ProductRepository::class)->findByUid($uid);
 	}
 }
