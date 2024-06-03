2f02ef721049d782cf3fbc4af38670557f03fd16
Christian Pschorr <pschorr.christian@gmail.com>
2024-06-03 23:08:22 +0200

    Übernahme Key facts Inhaltselement

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,67 +1,63 @@
 <?php
 
-// ---------------------------------------------------------------------------------------------------------------------
-// Weitere Felder in TT-Content
-$tmpEnityProductTtContentColumns = [
-	'tx_entityproduct_keyfacts' => [
-		'exclude' => true,
-		'label' => 'LLL:EXT:entity_product/Resources/Private/Language/locallang_tca.xlf:tt_content.tx_entityproduct_keyfacts',
-		'config' => [
-			'type' => 'inline',
-			'foreign_table' => 'tx_entityproduct_domain_model_keyfact',
-			'foreign_field' => 'content',
-			'foreign_sortby' => 'sorting',
-			'foreign_label' => 'title',
-			'maxitems' => 999,
-			'appearance' => [
-				'collapseAll' => 1,
-				'expandSingle' => 1,
-				'showAllLocalizationLink' => 1,
-				'showSynchronizationLink' => 1,
-				'showPossibleLocalizationRecords' => 1,
-				'showRemovedLocalizationRecords' => 1,
-				'newRecordLinkAddTitle' => 1
-			],
+(function () {
+
+	// -------------------------------------------------------------------------------------------------------------------
+	// Weitere Felder in Content
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', [
+		'tx_entityproduct_keyfacts' => [
+			'exclude' => true,
+			'label' => 'LLL:EXT:entity_product/Resources/Private/Language/locallang_tca.xlf:tt_content.tx_entityproduct_keyfacts',
+			'config' => [
+				'type' => 'inline',
+				'foreign_table' => 'tx_entityproduct_domain_model_keyfact',
+				'foreign_field' => 'content',
+				'foreign_sortby' => 'sorting',
+				'foreign_label' => 'title',
+				'maxitems' => 999,
+				'appearance' => [
+					'collapseAll' => 1,
+					'expandSingle' => 1,
+					'showAllLocalizationLink' => 1,
+					'showSynchronizationLink' => 1,
+					'showPossibleLocalizationRecords' => 1,
+					'showRemovedLocalizationRecords' => 1,
+					'newRecordLinkAddTitle' => 1
+				],
+			]
+		],
+	]);
+
+	// -------------------------------------------------------------------------------------------------------------------
+	// Modul in TYPO3 tt_content registrieren
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
+		array(
+			'LLL:EXT:entity_product/Resources/Private/Language/locallang_plugin.xlf:keyfact.title',
+			'entityproduct_keyfacts',
+			'entity-product-plugin'
+		),
+		'CType',
+		'entity_product'
+	);
+
+	// -------------------------------------------------------------------------------------------------------------------
+	// Definition Content Element
+	$GLOBALS['TCA']['tt_content']['types']['entityproduct_keyfacts']['showitem'] = \Ps14\Site\Service\TcaService::getShowitem(
+		['general', 'appearance', 'language', 'access', 'categories', 'notes', 'extended'],
+		[
+			'general' => '--palette--;;general, --palette--;;headers, --palette--;;foundation_identifier, tx_entityproduct_keyfacts,'
 		]
-	],
-];
+	);
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', $tmpEnityProductTtContentColumns);
-
-// ---------------------------------------------------------------------------------------------------------------------
-// Definition Content Element
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
-	array(
-		'LLL:EXT:entity_product/Resources/Private/Language/locallang_plugin.xlf:tx_entity_product_keyfact.name',
-		'entityproduct_keyfacts',
-		'ps14-content-keyfacts'
-	),
-	'CType',
-	'entity_product'
-);
-
-$GLOBALS['TCA']['tt_content']['types']['entityproduct_keyfacts'] = [
-	'showitem' => '
-			--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.general;general,
-			--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.header;xoHeader, tx_entityproduct_keyfacts,
-		--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.appearance,
-			--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.frames;frames,
-		--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.access,
-			--palette--;;hidden,
-			--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.visibility;visibility,
-			--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.access;access,
-		--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.extended
-	',
-];
-
-// ---------------------------------------------------------------------------------------------------------------------
-// Frontend Plugin Einstellungen
-
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['entityproduct_frontend'] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('entityproduct_frontend', 'FILE:EXT:entity_product/Configuration/FlexForms/Plugins/Frontend.xml');
-
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['entityproduct_keyfact'] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('entityproduct_keyfact', 'FILE:EXT:entity_product/Configuration/FlexForms/Plugins/KeyFact.xml');
-
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['entityproduct_teaser'] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('entityproduct_teaser', 'FILE:EXT:entity_product/Configuration/FlexForms/Plugins/Teaser.xml');
+	// -------------------------------------------------------------------------------------------------------------------
+	// Frontend Plugin Einstellungen
+	// @todo v12
+	//$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['entityproduct_frontend'] = 'pi_flexform';
+	//\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('entityproduct_frontend', 'FILE:EXT:entity_product/Configuration/FlexForms/Plugins/Frontend.xml');
+	//
+	//$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['entityproduct_keyfact'] = 'pi_flexform';
+	//\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('entityproduct_keyfact', 'FILE:EXT:entity_product/Configuration/FlexForms/Plugins/KeyFact.xml');
+	//
+	//$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['entityproduct_teaser'] = 'pi_flexform';
+	//\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('entityproduct_teaser', 'FILE:EXT:entity_product/Configuration/FlexForms/Plugins/Teaser.xml');
+})();
