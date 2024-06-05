fe16cafd49e4621bca64d2395209667ca1a74e06
Christian Pschorr <pschorr.christian@gmail.com>
2024-06-05 23:13:51 +0200

    Bereinigung Todo

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -51,7 +51,6 @@
 
 	// -------------------------------------------------------------------------------------------------------------------
 	// Frontend Plugin Einstellungen
-	// @todo v12
 	$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['entityproduct_frontend'] = 'pi_flexform';
 	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('entityproduct_frontend', 'FILE:EXT:entity_product/Configuration/FlexForms/Plugins/Frontend.xml');
 
