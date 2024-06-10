e18c8f805e13ea55d1b080271805316f5b83084e
Christian Pschorr <pschorr.christian@gmail.com>
2024-06-10 21:21:06 +0200

    Kleinere Behebungen für die Einbindung der Chart Extension

--- Classes/Provider/ChartDataValueProvider.php
+++ Classes/Provider/ChartDataValueProvider.php
@@ -3,6 +3,7 @@
 namespace Ps\EntityProduct\Provider;
 
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use FluidTYPO3\Flux\Provider\ProviderInterface;
 use FluidTYPO3\Flux\Form\Transformation\FormDataTransformer;
 use FluidTYPO3\Flux\Service\WorkspacesAwareRecordService;
 use FluidTYPO3\Flux\Builder\ViewBuilder;
